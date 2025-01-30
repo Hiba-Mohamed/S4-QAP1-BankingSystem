eeb1f79f59acae63ff26b40a34328fe2ab9e6e97
Hiba-Mohamed <hibamoha987@gmail.com>
2025-01-30 18:49:43 -0330

    secon test to test depositing

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -3,13 +3,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AccountTest {
+    Account account1 = new Account("cutomerycustomer", 100.00);
+
     @Test
     public void testCreationOfAccount(){
-        Account account1 = new Account("cutomerycustomer", 100.00);
         //want to test the name is the same
         Assertions.assertEquals( "cutomerycustomer", account1.customerName);
         //make sure the account balance the same
         Assertions.assertEquals(100.00, account1.accountBalance);
+    }
+
+    @Test
+    public void testDepositToAccount(){
+        account1.depositToAccount(200.00);
+        Assertions.assertEquals(300.00, account1.accountBalance);
     }
 
 }
